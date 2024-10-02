4f5f992123c80e936181e997eec3a501ab0d995d
Darwinist44 <79456459+Darwinist44@users.noreply.github.com>
2024-10-02 19:58:59 +0200

    middlewares added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,32 +3,41 @@ import express, { Request } from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import cors from "cors";
+import compression from "compression";
 
 import routes from "./routes/index.route";
 import { StatusCodes } from "http-status-codes";
+import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 var corsOptions = {
-  origin: "*",
+	origin: "*",
 };
+
+const limiter = rateLimit({
+	windowMs: 1000,
+	limit: 1,
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(limiter);
+//app.use(compression());
 
 app.use("/api", routes);
 
 mongoose
-  .connect(process.env.MONGO_URL || "")
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Running on: http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+	.connect(process.env.MONGO_URL || "")
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Running on: http://localhost:${PORT}`);
+		});
+	})
+	.catch((err) => console.log(err));
 
 app.get("*", (req: Request, res, next) => {
-  res.sendStatus(StatusCodes.NOT_FOUND);
+	res.sendStatus(StatusCodes.NOT_FOUND);
 });
--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
-import { getTasks, addTask } from "../controllers/task.controller";
+import { getTasks, addTask, deleteTask } from "../controllers/task.controller";
 
 const router = express.Router();
 
 router.get("/", getTasks);
 
 router.post("/", addTask);
+
+router.delete("/:id", deleteTask);
+
+//router.put("/", addTask);
 
 export default router;
